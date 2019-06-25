b35c62594143027b4be90709e9ab216a8278dafa
Kristof Goossens <kgoossens@MacBook-Pro-van-Kristof.local>
2019-06-25 23:51:48 +0200

    remove newlines before "pull" command

--- createnimbusapp.sh
+++ createnimbusapp.sh
@@ -13,13 +13,10 @@ nimbusapp aos:1.1.7 pull
 nimbusapp sv:5.0.1 pull
 nimbusapp ssc:18.10 pull
 nimbusapp sca:18.20 pull
-nimbusapp ppm-octane:9.50-12.60.21.98
- pull
+nimbusapp ppm-octane:9.50-12.60.21.98 pull
 nimbusapp mc:3.1 pull
-nimbusapp leanft-chrome:14.52
- pull
-nimbusapp da:6.2.1
- pull
+nimbusapp leanft-chrome:14.52 pull
+nimbusapp da:6.2.1 pull
 nimbusapp autopass:10.8.0 pull
 nimbusapp auditworkbench:18.20 pull
 nimbusapp ppm:9.51 pull
